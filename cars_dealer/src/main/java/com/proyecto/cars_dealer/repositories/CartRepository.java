ca0931f8b3fd6866b9b965101ff4d91f1101a16d
Juan Alberto <jarodriguo@eafit.edu.co>
2025-05-17 16:03:48 -0500

    Repositorio de las Reviews implementado (Para la base de datos)

--- cars_dealer/src/main/java/com/proyecto/cars_dealer/repositories/CartRepository.java
+++ cars_dealer/src/main/java/com/proyecto/cars_dealer/repositories/CartRepository.java
@@ -3,9 +3,11 @@ package com.proyecto.cars_dealer.repositories;
 import com.proyecto.cars_dealer.models.Cart;
 import com.proyecto.cars_dealer.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUser(User user);
 }
---
+++ cars_dealer/src/main/java/com/proyecto/cars_dealer/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.proyecto.cars_dealer.repositories;
+
+import com.proyecto.cars_dealer.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
